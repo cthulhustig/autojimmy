63a1a14684bb601e6f7326ec180f911495ec39fd
cthulhustig <63482768+cthulhustig@users.noreply.github.com>
2025-05-22 01:23:37 +0100

    fix: exception in dead space routing

--- logic/routeplanner.py
+++ logic/routeplanner.py
@@ -744,7 +744,7 @@ class RoutePlanner(object):
                         hexData.get(targetHex, (None, None, None))
                     isBetter = (nearbyHexBestFuelParsecs == None) or \
                         (fuelParsecs > nearbyHexBestFuelParsecs) or \
-                        (nearbyHex not in closedSet)
+                        (targetHex not in closedSet)
                     if isBetter:
                         yield (
                             targetHex,
